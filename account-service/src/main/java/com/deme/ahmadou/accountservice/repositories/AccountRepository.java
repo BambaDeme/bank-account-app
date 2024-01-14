e847c39113432b843ade429afae0027ff5f06f84
bambademe <cheikhbambademe@gmail.com>
2024-01-14 13:00:46 +0000

    gateway service with static routes configuration

--- account-service/src/main/java/com/deme/ahmadou/accountservice/repositories/AccountRepository.java
+++ account-service/src/main/java/com/deme/ahmadou/accountservice/repositories/AccountRepository.java
@@ -2,6 +2,8 @@ package com.deme.ahmadou.accountservice.repositories;
 
 import com.deme.ahmadou.accountservice.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
 }
