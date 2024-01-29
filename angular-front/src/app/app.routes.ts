e83b2d62084acefff5d2f12952558488d9636788
bambademe <cheikhbambademe@gmail.com>
2024-01-29 23:48:57 +0000

    add routes

--- angular-front/src/app/app.routes.ts
+++ angular-front/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { AccountsComponent } from './accounts/accounts.component';
 import { CustomersComponent } from './customers/customers.component';
 
 export const routes: Routes = [
-    {path: "customers", component:CustomersComponent},
-    {path: "accounts", component:AccountsComponent}
+    {path: 'customers', component: CustomersComponent},
+    {path: 'accounts', component: AccountsComponent}
 ];
