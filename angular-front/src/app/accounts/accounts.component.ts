f08535eae855932e9cab99bd20c8b99b60bbfccb
bambademe <cheikhbambademe@gmail.com>
2024-02-04 13:37:24 +0000

    account component

--- angular-front/src/app/accounts/accounts.component.ts
+++ angular-front/src/app/accounts/accounts.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accounts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './accounts.component.html',
   styleUrl: './accounts.component.css'
 })
-export class AccountsComponent {
+export class AccountsComponent implements OnInit {
 
+  accounts: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:8888/ACCOUNT-SERVICE/api/v1/accounts").subscribe({
+      next: data => {
+          this.accounts = data;
+      },
+      error: err => {
+        console.log(err)
+      }
+    })
+  }
 }
